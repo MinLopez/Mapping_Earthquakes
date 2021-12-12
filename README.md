57d2b1ebd1ef7479c74beb457c1953a567919206
MinLopez <mmaga006@gmail.com>
2021-12-12 00:17:50 -0800

    adding challenge files

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Mapping_Earthquakes
+Mapping Earthquakes in the last week, along with tectonic plates, and a layer for just earthquakes over 4.5 in the last week.
+Several layers including light, dark, satellite, and street views.
+
+
+## Overview of Project
+Create a map that displays earthquake data from the last week. Include borders for tectonic plates and color ranges for the intensity of earthquakes.
+
+## Results
+![Resources/TectonicAll.png](Resources/TectonicAll.png) 
+
+*Map showing **all earthquake data** for the last week and **tectonic borders***
+
+
+![Resources/TectonicMajor.png](Resources/TectonicMajor.png) 
+
+*Map showing **earthquake data over 4.5** for the last week and **tectonic borders***
+Excludes all data although it is available.
+***Note:* Because there were no earthquakes over 6, I changed the criteria from 6 to 5.5.**
+
+![Resources/DarkLayer.png](Resources/DarkLayer.png) 
+
+*Map showing the same as above **earthquake data over 4.5** for the last week and **tectonic borders** with a dark map base*
--- archive/Routes_Neighborhoods/static/js/logic.js
+++ archive/Routes_Neighborhoods/static/js/logic.js
@@ -13,7 +13,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 
 // We create the streets view tile layer that will be an option for our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -26,6 +26,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
+
 // Create a base layer that holds both maps.
 let baseMaps = {
     Light: light,
@@ -34,92 +35,72 @@ let baseMaps = {
     Satellite: satelliteStreets
   };
   
+
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
     center: [39.5, -98.5],
     zoom: 3,
-    layers: [streets]
+    layers: [light]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 
-// Accessing the earthquake
-let quakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/MinLopez/Mapping_Earthquakes/main/static/js/majorAirports.json";
 
-// This function returns the style data for each of the earthquakes we plot on
-// the map. We pass the magnitude of the earthquake into two separate functions
-// to calculate the color and radius.
-function styleInfo(feature) {
-  return {
-    opacity: 1,
-    fillOpacity: 1,
-    fillColor: getColor(feature.properties.mag),
-    color: "#000000",
-    radius: getRadius(feature.properties.mag),
-    stroke: true,
-    weight: 0.5
-  };
-}
-// This function determines the color of the circle based on the magnitude of the earthquake.
-function getColor(magnitude) {
-  if (magnitude > 5) {
-    return "#ea2c2c";
-  }
-  if (magnitude > 4) {
-    return "#ea822c";
-  }
-  if (magnitude > 3) {
-    return "#ee9c00";
-  }
-  if (magnitude > 2) {
-    return "#eecc00";
-  }
-  if (magnitude > 1) {
-    return "#d4ee00";
-  }
-  return "#98ee00";
+
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/MinLopez/Mapping_Earthquakes/main/static/js/torontoRoutes.json";
+
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/MinLopez/Mapping_Earthquakes/main/static/js/torontoNeighborhoods.json";
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
 }
 
-
-
-let magnitude = features.properties.mag;
-
-// This function determines the radius of the earthquake marker based on its magnitude.
-// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
-function getRadius(magnitude) {
-  if (magnitude === 0) {
-    return 1;
-  }
-  return magnitude * 4;
+let hoodStyle = {
+  color: "blue",
+  weight: 1,
+  fillColor: "yellow"
 }
+// Grabbing our GeoJSON data for airports
+d3.json(airportData, function(data) {
+    console.log(data);
+    // Creating a geoJSON layer with the retrieved data
+    L.geoJson(data, {
+      onEachFeature: function(features, layer){
+        layer.bindPopup("<h3> Airport Code: " + features.properties.faa + "</h3><hr> <h4>Airport name: " + features.properties.name + "</h4>")  
+      }
+    }).addTo(map);
+});
 
-// Create the earthquake layer for our map.
-let earthquakes = new L.layerGroup();
-
-
-// Retrieve the earthquake GeoJSON data.
-d3.json(quakeData, function(data) {
+// Grabbing our GeoJSON data for routes
+d3.json(torontoData, function(data) {
   console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
-  // We turn each feature into a circleMarker on the map.
-    pointToLayer: function(feature, latlng) {
-      console.log(data);
-      return L.circleMarker(latlng);
-    },
-  // We set the style for each circleMarker using our styleInfo function.
-  style: styleInfo,
-    // We create a popup for each circleMarker to display the magnitude and
-    //  location of the earthquake after the marker has been created and styled.
-    onEachFeature: function(feature, layer) {
-    layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-}
-}).addTo(map);
+    style: myStyle,
+    onEachFeature: function(features, layer){
+      layer.bindPopup("<h3> Airline:" + features.properties.airline + "</h3><hr><h4> Destination:" + features.properties.dst + "</h4>");
+    }
+  }).addTo(map);
+});
 
-// We define an object that contains the overlays.
-// This overlay will be visible all the time.
-let overlays = {
-  Earthquakes: earthquakes
-};});
+
+// Grabbing our GeoJSON data for neighborhoods
+d3.json(torontoHoods, function(data) {
+  console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+    style: hoodStyle,
+    onEachFeature: function(features, layer){
+      layer.bindPopup("<h3> Neighborhood:" + features.properties.AREA_NAME + "</h3>");
+    }
+  }).addTo(map);
+});
